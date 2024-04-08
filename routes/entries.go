e4d06b363de3925befb47d893c8c8790fe9b7ee6
Timothy Rodriguez <tim.rdrgz2017@live.com>
2024-04-07 22:46:15 -0500

    add entry complete

--- routes/entries.go
+++ routes/entries.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/timorodr/go-react-CRUD/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -21,6 +22,28 @@ var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
 func AddEntry(c *gin.Context) { // access to params and request through gin.Context
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 	var entry models.Entry
+
+	if err := c.BindJSON(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return // bind JSON serializes basically?
+	}
+	validationErr := validate.Struct(entry)
+	if validationErr != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": validationErr.Error()})
+		fmt.Println(validationErr)
+		return
+	}
+	entry.ID = primitive.NewObjectID()
+	result, insertErr := entryCollection.InsertOne(ctx, entry)
+	if insertErr != nil {
+		msg := fmt.Sprintf("order item was not created")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		fmt.Println(insertErr)
+		return
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result)
 }
 
 func GetEntries(c *gin.Context) {
@@ -73,7 +96,8 @@ func UpdateEntry(c *gin.Context) {
 }
 
 func UpdateIngredient(c *gin.Context) {
-
+	entryID := params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(entryID)
 }
 
 func DeleteEntry(c *gin.Context) {
